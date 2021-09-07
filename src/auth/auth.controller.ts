f85a475bbc794860414a5e71d008a327ba759fdf
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-09-08 00:36:35 +0900

    'ユーザーが１人しか作れないバグの修正'

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,6 +14,7 @@ export class AuthController {
 
   @Post('register')
   register(@Body(ValidationPipe) registUser: CreateUserDto) {
+    console.log(registUser, 9);
     return this.authService.regist(registUser);
   }
 }
