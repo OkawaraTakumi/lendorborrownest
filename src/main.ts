f08ff5700f8e59ae0b2716d73b88f51e70fa9abe
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-09-29 13:41:26 +0900

    corsの設定を変えてみた

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   await app.listen(5000);
 }
 bootstrap();
