50b3f01e31221bcbff00681f9e770cc6b47f225d
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-10-01 10:53:46 +0900

    helmetの導入

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet);
   app.use(cookieParser());
   app.enableCors({
     origin: 'https://lendorborrownext-movm5510o-okawaratakumi.vercel.app',
