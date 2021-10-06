a64b46588177f8257c882bc8443671071b4bc5ed
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-10-06 12:12:44 +0900

    更新

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     origin: [
-      'https://lendorborrownext-mkqgvi175-okawaratakumi.vercel.app',
+      'https://lendorborrownext-okawaratakumi.vercel.app/',
       'http://localhost:3000',
     ],
     methods: ['*', 'GET', 'POST', 'PUT', 'OPTIONS'],
